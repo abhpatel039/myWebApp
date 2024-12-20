cb9efe0d3486e711b2e9b6ad6e2face7b39b36a3
abhpate039 <abhpatel039@gmail>
2024-12-20 14:58:04 +0530

    Feature 1 is added to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//This is the change in code .
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
